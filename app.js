8be86d7f3ad06ea597fe62168b6857185f14f34b
iamamirali <iamamirali.sh@gmail.com>
2021-08-15 17:47:45 +0430

    add suitable item icons

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// choose country, which defaultly is iran
+// choose country, which iran by default
 let country = 'iran'
 
 function getData() {
     fetch(`https://corona.lmao.ninja/v2/countries/${country}`)
     .then(response => response.json())
     .then(data => htmlSetter(data))
+    .catch(err => console.log(err))
 }
 getData()
+
 
 // stats items
 const activeCases = document.querySelector('.active-cases')
@@ -23,18 +25,31 @@ const searchBtn = document.getElementById('search-btn')
 searchBar.value = ''
 
 // chosen country title value
-const countryTitle = 'Iran'
+const chosenCountry = document.querySelector('.chosen-country')
 
 // data number elt
 const statsNumber = document.querySelector('.stats-number')
 
+// after clicking srch btn, looks for data of typed country
 searchBtn.addEventListener('click', function(){
     country = searchBar.value.toLowerCase()
     getData()
 })
 
-// sets suitable value for different items
+// search by enter key button
+searchBar.addEventListener("keyup", function(event) {
+    // number 13 is the enter key on the keyboard
+    if (event.keyCode === 13) {
+      // does searchBtn click action by clicking enter btn
+    searchBtn.click();
+    }
+}); 
+
 function htmlSetter(data) {
+    // sets name of the country in the title
+    chosenCountry.textContent = data.country
+
+    // sets suitable value for different items
     itemValueSetter(activeCases,data.todayCases)
     itemValueSetter(criticalCases,data.critical)
     itemValueSetter(recoveredCases,data.todayRecovered)
@@ -45,5 +60,5 @@ function htmlSetter(data) {
 
 // gets data, creates p element and put the specified value to specified container
 function itemValueSetter(item,data) {
-        item.children[2].textContent = data
+    item.children[2].textContent = data
 }
